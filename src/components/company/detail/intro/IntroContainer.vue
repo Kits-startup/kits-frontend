35ea23f14f5e295cff7ee5d44f18c51b49d634e7
hamin <alrbsshim14@gmail.com>
2022-09-16 21:01:47 +0900

    write review flow done

--- src/components/company/detail/intro/IntroContainer.vue
+++ src/components/company/detail/intro/IntroContainer.vue
@@ -1,5 +1,116 @@
 <template>
   <div class="infoContainer">
+    <div class="reviewCompleteModal" v-if="showCompleteModal">
+      <div class="complete">
+        <img src="@/assets/check.png" alt="" />
+        <div
+          style="
+            font-weight: 500;
+            font-size: 32px;
+            line-height: 47px;
+            color: #000000;
+          "
+        >
+          리뷰 등록 완료
+        </div>
+        <div
+          style="
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 27px;
+            color: #515151;
+            margin-top: 7px;
+          "
+        >
+          리뷰 등록이 완료 되었습니다!
+        </div>
+      </div>
+    </div>
+    <b-modal id="reviewModal" hide-footer hide-header centered>
+      <div class="reviewModalContainer">
+        <div class="closeX" @click="$bvModal.hide('reviewModal')">X</div>
+        <div class="rateNTotal">
+          <div class="ratingBoxes">
+            <div class="ratingBox">
+              <div class="label">일과 삶의 균형</div>
+              <div class="starBox">
+                <div
+                  class="star"
+                  v-for="i in 5"
+                  v-bind:key="i"
+                  @click="rateChange_balance(i)"
+                  v-bind:class="i <= write_review.balance ? 'color' : null"
+                >
+                  ★
+                </div>
+              </div>
+            </div>
+            <div class="ratingBox">
+              <div class="label">보상과 복지</div>
+              <div class="starBox">
+                <div
+                  class="star"
+                  v-for="i in 5"
+                  v-bind:key="i"
+                  @click="rateChange_reward(i)"
+                  v-bind:class="i <= write_review.reward ? 'color' : null"
+                >
+                  ★
+                </div>
+              </div>
+            </div>
+            <div class="ratingBox">
+              <div class="label">동료와 문화</div>
+              <div class="starBox">
+                <div
+                  class="star"
+                  v-for="i in 5"
+                  v-bind:key="i"
+                  @click="rateChange_culture(i)"
+                  v-bind:class="i <= write_review.culture ? 'color' : null"
+                >
+                  ★
+                </div>
+              </div>
+            </div>
+          </div>
+          <span class="totalPotint"
+            >나의 점수:
+            <span class="blue"> {{ write_review.total.toFixed(1) }}</span></span
+          >
+        </div>
+        <div class="inputLabel">장점</div>
+        <textarea
+          name="pros"
+          id="pros"
+          class="proNCon"
+          cols="50"
+          placeholder="리뷰를 남겨주세요. (장점)"
+          v-model="write_review.pros"
+        />
+        <div class="letterNum">{{ write_review.pros.length }}/500</div>
+        <div class="inputLabel">단점</div>
+        <textarea
+          name="cons"
+          id="cons"
+          class="proNCon"
+          cols="50"
+          placeholder="리뷰를 남겨주세요. (단점)"
+          v-model="write_review.cons"
+        />
+        <div class="letterNum">{{ write_review.cons.length }}/500</div>
+        <div class="warn">ℹ️허위사실로 작성시 불이익이 가해질 수 있습니다.</div>
+        <div
+          class="review_submit"
+          @click="
+            $bvModal.hide('reviewModal');
+            submitReview();
+          "
+        >
+          리뷰 등록하기
+        </div>
+      </div>
+    </b-modal>
     <IntroInfoContainer :companyInfo="companyInfo" />
     <div class="rateContainer">
       <div class="rate">
@@ -10,12 +121,16 @@
               class="star"
               v-for="i in companyInfo.rateStarNum"
               v-bind:key="i"
-            ></div>
+            >
+              ⭐
+            </div>
           </div>
           <div class="reviewNum">리뷰 ({{ companyInfo.reviewNum }})</div>
         </div>
       </div>
-      <div class="submitBtn">리뷰 등록 하기</div>
+      <div class="submitBtn" @click="$bvModal.show('reviewModal')">
+        리뷰 등록 하기
+      </div>
     </div>
     <RateContainer :companyInfo="companyInfo" />
     <div class="reviewPageContainer">
@@ -151,6 +266,16 @@ export default {
       },
       clickedPage: 1,
       login: false,
+      reviewModalVisible: false,
+      write_review: {
+        balance: 0,
+        reward: 0,
+        culture: 0,
+        total: 0,
+        pros: "",
+        cons: "",
+      },
+      showCompleteModal: false,
     };
   },
   components: {
@@ -168,6 +293,37 @@ export default {
     },
     changeToLogin: function () {
       this.login = !this.login;
+    },
+    registerReview: function () {
+      this.reviewModalVisible = true;
+    },
+    rateChange_balance: function (rate) {
+      this.write_review.balance = rate;
+      this.write_review.total =
+        (this.write_review.balance +
+          this.write_review.reward +
+          this.write_review.culture) /
+        3;
+    },
+    rateChange_reward: function (rate) {
+      this.write_review.reward = rate;
+      this.write_review.total =
+        (this.write_review.balance +
+          this.write_review.reward +
+          this.write_review.culture) /
+        3;
+    },
+    rateChange_culture: function (rate) {
+      this.write_review.culture = rate;
+      this.write_review.total =
+        (this.write_review.balance +
+          this.write_review.reward +
+          this.write_review.culture) /
+        3;
+    },
+    submitReview: function () {
+      this.showCompleteModal = true;
+      setTimeout(() => this.$router.push("home"), 3000);
     },
   },
 };
@@ -337,6 +493,117 @@ export default {
     font-size: 16px;
     line-height: 24px;
     color: #0376db;
+  }
+}
+#reviewModal {
+  .reviewModalContainer {
+    padding: 20px;
+  }
+  position: relative;
+  .rateNTotal {
+    display: flex;
+  }
+  .ratingBoxes {
+    width: 74%;
+  }
+  .ratingBox {
+    display: flex;
+    margin-top: 23px;
+  }
+  .label {
+    width: 50%;
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 33px;
+    color: #0376db;
+  }
+  .starBox {
+    display: flex;
+  }
+  .star {
+    font-size: 22px;
+    color: gray;
+  }
+  .color {
+    color: #f1c40f;
+  }
+  .totalPotint {
+    align-self: flex-end;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: #878787;
+    .blue {
+      color: #0376db;
+    }
+  }
+  .closeX {
+    position: absolute;
+    top: 30px;
+    right: 30px;
+    cursor: pointer;
+  }
+  .inputLabel {
+    margin-top: 30px;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 27px;
+    color: #878787;
+    margin-bottom: 3px;
+  }
+  .proNCon {
+    border: 1px solid #878787;
+    width: 100%;
+    height: 125px;
+    border-radius: 5px;
+    padding: 10px 16px;
+  }
+  .letterNum {
+    display: flex;
+    justify-content: flex-end;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 21px;
+    color: #878787;
+  }
+  .warn {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  .review_submit {
+    background: #0376db;
+    border-radius: 29.5px;
+    padding: 15px 50px;
+    width: max-content;
+    margin: auto;
+    color: white;
+    cursor: pointer;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 27px;
+  }
+}
+.reviewCompleteModal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgb(85, 85, 85);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .complete {
+    background: white;
+    width: 628px;
+    height: 275px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+    justify-content: center;
   }
 }
 </style>
