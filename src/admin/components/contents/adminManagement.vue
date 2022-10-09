b0953fd0ef15881ee3f253fdfa9b261c3af13578
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2022-10-09 14:05:23 +0900

    sub page 잘 작동하게끔 구현

--- src/admin/components/contents/adminManagement.vue
+++ src/admin/components/contents/adminManagement.vue
@@ -9,12 +9,20 @@ export default {
       tab: null,
     };
   },
-  watch: {
-    $route() {
+  methods: {
+    setTab() {
       console.log("change!");
       const url = this.$route.fullPath.split("/");
       console.log(url.slice(-1));
       this.tab = url.slice(-1)[0];
+    },
+  },
+  mounted() {
+    this.setTab();
+  },
+  watch: {
+    $route() {
+      this.setTab();
     },
   },
 };
--- src/admin/components/contents/paymentManagement.vue
+++ src/admin/components/contents/paymentManagement.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>결제 관리!</div>
+  <div>결제 관리!{{ tab }}</div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tab: null,
+    };
+  },
+  methods: {
+    setTab() {
+      console.log("change!");
+      const url = this.$route.fullPath.split("/");
+      console.log(url.slice(-1));
+      this.tab = url.slice(-1)[0];
+    },
+  },
+  mounted() {
+    this.setTab();
+  },
+  watch: {
+    $route() {
+      this.setTab();
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
