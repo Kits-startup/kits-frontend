b2affe7b482173bdf08f0c0effe565a2b2499f5d
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2022-10-11 13:47:52 +0900

    theme job posting

---
+++ src/components/mainPage/eachPosting.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="eContainer">
+    <div class="thumbnail">
+      <img class="kImage" :src="exam.src" alt="thumbnail" />
+      <div class="heart">
+        <img class="logo" src="@/assets/favorite_border.png" alt="heart" />
+      </div>
+    </div>
+    <div class="info">
+      <div class="name">{{ exam.name }}</div>
+      <div class="subscription">
+        <span class="category">{{ exam.category }}</span
+        ><span class="location">{{ exam.location }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      exam: {
+        src: "https://picsum.photos/1024/480/?image=52",
+        name: "아비즈",
+        category: "온라인 쇼핑몰",
+        location: "경기 한국",
+      },
+    };
+  },
+  props: {
+    companyInfo: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.eContainer {
+  width: 100%;
+  margin-bottom: 90px;
+  .thumbnail {
+    position: relative;
+    border-radius: 15px;
+    overflow: hidden;
+    margin-bottom: 17px;
+    .heart {
+      position: absolute;
+      top: 15px;
+      right: 15px;
+    }
+  }
+  .kImage {
+    height: 234px;
+    width: 100%;
+  }
+  .info {
+    .name {
+      font-size: 22px;
+      line-height: 30px;
+      margin-bottom: 8px;
+    }
+    .category {
+      font-size: 20px;
+      line-height: 27px;
+    }
+    .location {
+      font-size: 16px;
+      line-height: 22px;
+      color: #9b9b9b;
+      margin-left: 13px;
+    }
+  }
+}
+</style>
---
+++ src/components/mainPage/themePosting.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="themeContainer">
+    <div class="header">
+      <div class="title">테마로 보는 요즘 채용</div>
+      <span>더보기</span>
+    </div>
+    <div class="contentContainer">
+      <b-row>
+        <b-col cols="3" v-for="(item, index) in 8" :key="index">
+          <each-posting-vue />
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import eachPostingVue from "./eachPosting.vue";
+export default {
+  components: {
+    eachPostingVue,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.themeContainer {
+  padding-top: 100px;
+  width: 1300px;
+  margin: auto;
+}
+.header {
+  display: flex;
+  align-items: center;
+  .title {
+    text-align: center;
+    margin: auto;
+    font-size: 32px;
+    line-height: 44px;
+  }
+  span {
+    text-align: right;
+    font-size: 20px;
+    line-height: 27px;
+  }
+  margin-bottom: 40px;
+}
+.contentContainer {
+}
+</style>
