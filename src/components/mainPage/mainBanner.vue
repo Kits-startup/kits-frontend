55553813a69e4aa6a45e1f5f48d2945ed3670242
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2022-10-11 13:47:42 +0900

    banner... not found library

---
+++ src/components/mainPage/mainBanner.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bannerContainer">
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="10000"
+      background="#ababab"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide
+        v-for="i in 9"
+        :key="i"
+        :caption="i"
+        img-src="https://picsum.photos/1024/480/?image=52"
+        img-width="740"
+        img-height="400"
+      />
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.bannerContainer {
+  width: 740px;
+  border: 1px solid black;
+  margin: auto;
+}
+</style>
