ef2ecb3775a5461a30dd4498a34fe6bfad5bc2c2
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2022-11-05 20:20:14 +0900

    initial setting

--- src/admin/components/contents/user/userManagement.vue
+++ src/admin/components/contents/user/userManagement.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>회원 관리!{{ tab }}</div>
+  <div>
+    회원 관리 > 홈 > 회원관리 > 회원목록
+    <user-list v-if="tab.includes('list')" />
+    <Approval_userList v-if="tab.includes('register')" />
+  </div>
 </template>
 
 <script>
+import Approval_userList from "./approval_userList.vue";
+import userList from "./userList.vue";
 export default {
+  components: { userList, Approval_userList },
   data() {
     return {
       tab: null,
@@ -11,10 +18,10 @@ export default {
   },
   methods: {
     setTab() {
-      console.log("change!");
       const url = this.$route.fullPath.split("/");
-      console.log(url.slice(-1));
-      this.tab = url.slice(-1)[0];
+      console.log(url);
+      this.tab = url[3];
+      console.log(this.tab);
     },
   },
   mounted() {
