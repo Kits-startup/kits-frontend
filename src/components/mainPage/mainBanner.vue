65fc5db5ca7823d66f5ffdec18cfd37cd249fffc
hamin <alrbsshim14@gmail.com>
2022-12-26 16:08:14 +0900

    banner change

--- src/components/mainPage/mainBanner.vue
+++ src/components/mainPage/mainBanner.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="bannerContainer">
     <b-carousel
-      id="carousel-1"
+      id="banner"
       v-model="slide"
-      :interval="10000"
+      :interval="5000"
       background="#ababab"
       style="text-shadow: 1px 1px 2px #333"
+      indicators
+      fade
     >
-      <b-carousel-slide
-        v-for="i in 9"
-        :key="i"
-        :caption="i.toString()"
-        img-src="https://picsum.photos/1024/480/?image=52"
-        img-width="740"
-        img-height="400"
-      />
+      <b-carousel-slide v-for="(e, i) in slideInfo" :key="i">
+        <template #img>
+          <img
+            class=""
+            width="1324"
+            height="480"
+            :src="e.src"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
     </b-carousel>
   </div>
 </template>
@@ -22,7 +27,21 @@
 <script>
 export default {
   data() {
-    return { slide: null };
+    return {
+      slide: null,
+      slideInfo: [
+        {
+          src: "https://cdn.travie.com/news/photo/first/201705/img_19694_12.jpg",
+        },
+        {
+          src: "https://mblogthumb-phinf.pstatic.net/MjAxODAzMzFfMTUg/MDAxNTIyNDg4NDQ1ODAx.SykBPNfjfWsBXyo87j56KVJp_qiaLuyUUOJQjcKRotwg.DXrnutpLdfHr_ly8rh5Ne3xEooJbmBEdqegntXo_7i0g.JPEG.myrikason/KakaoTalk_Moim_5qpBqAjsVPmB3swqW7wgUdr3kBGruh.jpg?type=w800",
+        },
+        { src: "https://t1.daumcdn.net/cfile/tistory/206FFA024CDA899150" },
+        {
+          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAVWjQ0HcLm7p0PJWCS2XTYPlfwpmQ2Hp_UA&usqp=CAU",
+        },
+      ],
+    };
   },
   components: {},
 };
@@ -30,8 +49,11 @@ export default {
 
 <style lang="scss" scoped>
 .bannerContainer {
-  width: 740px;
-  border: 1px solid black;
+  width: 1324px;
   margin: auto;
+}
+#banner {
+  height: 480px;
+  width: 100%;
 }
 </style>
